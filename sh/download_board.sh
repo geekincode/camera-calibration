b2c17390ee207bb83173c7f048b6b87a3c7292ab
geekincode <2949542342@qq.com>
2025-07-06 17:50:44 +0800

    fix_test

--- sh/download_board.sh
+++ sh/download_board.sh
@@ -9,7 +9,7 @@ echo "当前用户为：$USER"
 while IFS= read -r -n1 char; do
     echo -n "$char"
     sleep 0.1
-done <<< "当前用户为：$USER"
+# done <<< "当前用户为：$USER"
 echo "ROS系统版本: "${ROS_DISTRO}
 
 if [ ! -d "sh" ]; then
@@ -29,7 +29,7 @@ if [ ! -d "tools" ]; then
   mkdir tools
 fi
 
-$SUDO wget -q https://raw.githubusercontent.com/geekincode/camera-calibration/refs/heads/main/src/auto.py -O src/auto.py
+wget -q https://raw.githubusercontent.com/geekincode/camera-calibration/refs/heads/main/src/auto.py -O src/auto.py
 $SUDO python3 src/auto.py
 
 xdg-open ./download/calib.io_checker_200x150_8x11_15.pdf
